05659336dde90dd876608f5a06062d8ccb5349eb
Baer Lanfried <blanfried@yahoo.com>
2022-11-21 12:26:21 -0800

    added a current date (day) to be displayed at top of page along with time of day

--- Assets/index.js
+++ Assets/index.js
@@ -12,3 +12,47 @@ if (localStorage.getItem("textSchedule") === null) {
     newItem.setAttribute("value", textContent);
   });
 }
+
+
+function currentDate() {
+      let date = moment();
+      $("#currentDay").text("Today is : " + date.format("dddd"));
+    }
+    
+    timeOfDay = {
+      0: 9,
+      1: 10,
+      2: 11,
+      3: 12,
+      4: 13,
+      5: 14,
+      6: 15,
+      7: 16,
+      8: 17,
+    };
+    function checkTime() {
+      let listScheduleAr = document.querySelectorAll(".blockListItem");
+      let currentTimeRaw = moment();
+      let currentTime = currentTimeRaw.format("HH");
+      Object.values(timeOfDay).forEach((hour, i) => {
+        if (hour < currentTime) {
+          let count = i + 1;
+          $(".blockListItem:nth-child(" + count + ")").css(
+            "background-color",
+            "#a6a6a6"
+          );
+        } else if (hour == currentTime) {
+          let count = i + 1;
+          $(".blockListItem:nth-child(" + count + ")").css(
+            "background-color",
+            "#f5b0ab"
+          );
+        } else {
+          let count = i + 1;
+          $(".blockListItem:nth-child(" + count + ")").css(
+            "background-color",
+            "#b7f7bc"
+          );
+        }
+      });
+    }
